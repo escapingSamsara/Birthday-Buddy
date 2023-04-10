8a6d20eee29c695e7f36eba739506a9457222390
escapingSamsara <jpusterhofer@gmail.com>
2023-04-10 12:29:35 +0200

    unused css removed

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -4,7 +4,7 @@ const Person = ({ name, age, image }) => {
       <img src={image} alt={name} className="img" />
       <div>
         <h4>{name}</h4>
-        <p>{age} Years</p>
+        <p style={{ margin: '0.1rem' }}>{age} Years</p>
       </div>
     </article>
   )
